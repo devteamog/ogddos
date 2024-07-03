addac533a488d5f859554cf2c34474d3efddce18
devteamog <165029448+devteamog@users.noreply.github.com>
2024-07-03 16:55:40 +0530

    Update m.py

--- m.py
+++ m.py
@@ -206,7 +206,7 @@ def start_attack_reply(message, target, port, time):
     user_info = message.from_user
     username = user_info.username if user_info.username else user_info.first_name
     
-    response = f"{username}, 𝐀𝐓𝐓𝐀𝐂𝐊 𝐒𝐓𝐀𝐑𝐓𝐄𝐃.\n\n𝐓𝐚𝐫𝐠𝐞𝐭: {target}\n𝐏𝐨𝐫𝐭: {port}\n𝐓𝐢𝐦𝐞: {time} 𝐒𝐞𝐜𝐨𝐧𝐝𝐬\n𝐌𝐞𝐭𝐡𝐨𝐝: BGMI\nBy danav0 @danav0"
+    response = f"{username}, 𝐀𝐓𝐓𝐀𝐂𝐊 𝐒𝐓𝐀𝐑𝐓𝐄𝐃.\n\n𝐓𝐚𝐫𝐠𝐞𝐭: {target}\n𝐏𝐨𝐫𝐭: {port}\n𝐓𝐢𝐦𝐞: {time} 𝐒𝐞𝐜𝐨𝐧𝐝𝐬\n𝐌𝐞𝐭𝐡𝐨𝐝: BGMI\nBy OG @TeamOG9"
     bot.reply_to(message, response)
 
 # Dictionary to store the last time each user ran the /bgmi command
@@ -244,9 +244,9 @@ def handle_bgmi(message):
                 subprocess.run(full_command, shell=True)
                 response = f"BGMI Attack Finished. Target: {target} Port: {port} Time: {time}"
         else:
-            response = "Usage :- /bgmi <target> <port> <time>\nBy danav0 @danav0"  # Updated command syntax
+            response = "Usage :- /bgmi <target> <port> <time>\nBy OG @TeamOG9"  # Updated command syntax
     else:
-        response = "You Are Not Authorized To Use This Command.\nBy danav0 @danav0"
+        response = "You Are Not Authorized To Use This Command.\nBy OG @TeamOG9"
 
     bot.reply_to(message, response)
 
@@ -283,7 +283,7 @@ def show_help(message):
 
  To See Admin Commands:
  /admincmd : Shows All Admin Commands.
- By danav0 @danav0
+ By OG @TeamOG9
 '''
     for handler in bot.message_handlers:
         if hasattr(handler, 'commands'):
@@ -298,7 +298,7 @@ def show_help(message):
 @bot.message_handler(commands=['start'])
 def welcome_start(message):
     user_name = message.from_user.first_name
-    response = f"Welcome to Your Home, {user_name}! Feel Free to Explore.\nTry To Run This Command : /help\nWelcome To The World's Best Ddos Bot\nBy danav0 @danav0"
+    response = f"Welcome to Your Home, {user_name}! Feel Free to Explore.\nTry To Run This Command : /help\nWelcome To The World's Best Ddos Bot\nBy OG @TeamOG9"
     bot.reply_to(message, response)
 
 
@@ -310,7 +310,7 @@ def welcome_rules(message):
 1. Dont Run Too Many Attacks !! Cause A Ban From Bot
 2. Dont Run 2 Attacks At Same Time Becz If U Then U Got Banned From Bot. 
 3. We Daily Checks The Logs So Follow these rules to avoid Ban!!
-By danav0 @danav0'''
+By OG @TeamOG9'''
     bot.reply_to(message, response)
 
 @bot.message_handler(commands=['plan'])
@@ -327,7 +327,7 @@ Pr-ice List:
 Day-->150 Rs
 Week-->900 Rs
 Month-->1600 Rs
-By danav0 @danav0
+By OG @TeamOG9
 '''
     bot.reply_to(message, response)
 
@@ -342,7 +342,7 @@ def welcome_plan(message):
 /logs : All Users Logs.
 /broadcast : Broadcast a Message.
 /clearlogs : Clear The Logs File.
-By danav0 @danav0
+By OG @TeamOG9
 '''
     bot.reply_to(message, response)
 
@@ -373,4 +373,4 @@ def broadcast_message(message):
 
 
 bot.polling()
-#By danav0
+#By OG
